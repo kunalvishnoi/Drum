bee2115185a801c4990015d15cb3390547eabe77
kunalvishnoi <vishnoi.kunal0@gmail.com>
2017-07-26 19:59:44 +0530

    drum kit

---
+++ javascript.js
@@ -0,0 +1,45 @@
+document.addEventListener('keydown', function(e) {
+  if(e.keyCode == 49){
+  document.getElementById('yo').play();
+  }
+});
+document.addEventListener('keydown', function(e) {
+  if(e.keyCode == 50){
+  document.getElementById('yo1').play();
+  }
+});
+document.addEventListener('keydown', function(e) {
+  if(e.keyCode == 51){
+  document.getElementById('yo2').play();
+  }
+});
+document.addEventListener('keydown', function(e) {
+  if(e.keyCode == 52){
+  document.getElementById('yo3').play();
+  }
+});
+document.addEventListener('keydown', function(e) {
+  if(e.keyCode == 53){
+  document.getElementById('yo4').play();
+  }
+});
+document.addEventListener('keydown', function(e) {
+  if(e.keyCode == 54){
+  document.getElementById('yo5').play();
+  }
+});
+document.addEventListener('keydown', function(e) {
+  if(e.keyCode == 55){
+  document.getElementById('yo6').play();
+  }
+});
+document.addEventListener('keydown', function(e) {
+  if(e.keyCode == 56){
+  document.getElementById('yo7').play();
+  }
+});
+document.addEventListener('keydown', function(e) {
+  if(e.keyCode == 57){
+  document.getElementById('yo8').play();
+  }
+});
